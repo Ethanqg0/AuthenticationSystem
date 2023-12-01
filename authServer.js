6feaf0405415964801ca957239b9f71c6aa395ee
Ethan Gutierrez <Ethan.e.gutierrez@gmail.com>
2023-12-01 14:27:03 -0800

    adding refresh tokens and console logging for debugging purposes

---
+++ authServer.js
@@ -0,0 +1,74 @@
+/*
+  Why do we need a refresh token?
+
+  Our current tokens have no expiration date, so they are valid forever.
+  This is a security risk because if someone gets a hold of your token, they can use it forever.
+
+  1) invalidate users tokens that shouldnt have access
+  2) move your authentication separate to your main server
+*/
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const jwt = require('jsonwebtoken')
+
+app.use(express.json())
+
+// normally want to store refreshTokens in a database or a cache like redis
+// can you cache multiple tokens? yes, in a list
+// will be emptied out when the server restarts
+let refreshTokens = []
+
+/*
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token
+  if (refreshToken == null) return res.sendStatus(401) // if there is no refresh token, then it is not valid
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403) // if the refresh token is not in the list of refresh tokens, then it is not valid
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403)
+    const accessToken = generateAccessToken({ name: user.name })
+    res.json({ accessToken: accessToken })
+  })
+})
+*/
+
+function verifyToken(refreshToken, refreshTokens) {
+  if (refreshToken == null) return { status: 401 }; // if there is no refresh token, then it is not valid
+  if (!refreshTokens.includes(refreshToken)) return { status: 403 }; // if the refresh token is not in the list of refresh tokens, then it is not valid
+  let user;
+  try {
+    user = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+  } catch (err) {
+    return { status: 403 };
+  }
+  const accessToken = generateAccessToken({ name: user.name });
+  return { status: 200, accessToken: accessToken };
+}
+
+module.exports = verifyToken;
+
+
+
+app.delete('/logout', (req, res) => {
+  refreshTokens = refreshTokens.filter(token => token !== req.body.token)
+  res.sendStatus(204)
+})
+
+app.post('/login', (req, res) => {
+  // Authenticate User
+
+  const username = req.body.username
+  const user = { name: username }
+
+  const accessToken = generateAccessToken(user)
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET)
+  refreshTokens.push(refreshToken)
+  res.json({ accessToken: accessToken, refreshToken: refreshToken })
+})
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' })
+}
+
+app.listen(4000)
--- server.js
+++ server.js
@@ -33,38 +33,79 @@ const posts = [
   {
     username: 'Jim',
     title: 'Post 2'
+  },
+  {
+    username: 'Kyle',
+    title: 'Post 3'
+  },
+  {
+    username: 'Jim',
+    title: 'Post 4'
   }
 ];
-
-const validUsers = ["Kyle", "Jim"];
 
 app.get('/posts', authenticateToken, (req, res) => {
   res.json(posts);
 });
 
-app.post('/login', (req, res) => {
-  // Authenticate User
-
-  const username = req.body.username;
-  const user = { name: username };
-
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-  res.json({ accessToken: accessToken })
-}); 
-
-// middleware function
 function authenticateToken(req, res, next) {
-  // Bearer Token
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-  if (token == null ) return res.sendStatus(401);
+  console.log('Received token:', token);
+
+  if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403); // Forbidden, invalid token
-    req.user = user;
+    console.log('Error during verification:', err);
+    console.log('User:', user);
+
+    if (err) {
+      const result = verifyToken(token);
+      console.log('Verification result:', result);
+
+      if (result.status === 401) {
+        return res.sendStatus(401);
+      } else if (result.status === 403) {
+        return res.sendStatus(403);
+      }
+
+      req.user = result.user;
+
+      // Update the response with the new access token
+      res.setHeader('Authorization', 'Bearer ' + result.accessToken);
+    } else {
+      req.user = user;
+    }
     next();
-  })
+  });
 }
 
-app.listen(3000);
 
+function verifyToken(refreshToken) {
+  console.log('Verifying refresh token:', refreshToken);
+
+  if (refreshToken == null) return { status: 401 };
+
+  let user;
+  try {
+    user = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    console.log('Decoded user from refresh token:', user);
+  } catch (err) {
+    console.log('Error during refresh token verification:', err);
+    return { status: 403 };
+  }
+
+  const accessToken = generateAccessToken({ name: user.name });
+  console.log('Generated new access token:', accessToken);
+
+  return { status: 200, user: { name: user.name }, accessToken: accessToken };
+}
+
+
+function generateAccessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15m' });
+}
+
+
+
+app.listen(3000);
